339fc1ad74dcd2e9e571652a53415aa56672e776
Alexander-222-Mor <amorgunov948@gmail.com>
2024-10-14 21:49:45 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,7 @@
+total_task = 12
+time_spent = 1.5
+course_name = "python"
+print ( time_spent / total_task )
+time_of_one_task = ( time_spent / total_task )
+print('курс', course_name ,'всего задач', total_task ,'затрачено часов '\
+,time_spent,'среднее время выполнения ', time_of_one_task ,)
